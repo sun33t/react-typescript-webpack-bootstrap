cc262a0e6ad94b72341bee6ccf0adb1761c24747
Suneet Misra <hello@suneet.codes>
2021-07-31 08:42:07 +0100

    Added abilty to load css files and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import './styles.css';
+import IMAGE from './eat-sleep-code.jpg';
+
 const App = () => (
   <div>
     <h1>React TypeScript Bootstrap</h1>
+    <img src={IMAGE} alt="eat sleep code" />
   </div>
 );
 
